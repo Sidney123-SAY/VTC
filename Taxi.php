92deace33033bb1a265fd885764b790be8f128ea
Adolphe008 <aliouf326@gmail.com>
2025-05-30 23:28:23 +0200

    ajout de la page Voirprix,des styles CSS,et de l'image associée

---
+++ Taxi.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taxi</title>
+    <link rel="stylesheet" href="stylepagetaxi.css">
+</head>
+    <header>
+    <button class="logo">ASIH</button>
+    <a href="Accueil.php"><-Retour</a>
+</header>
+
+<body>
+ <div class="container"> 
+     <div class="left">
+     <h3>Commandez ou planifiez un taxi</h3>
+       <form action="footer.php" method="POST">
+         <input type="text" name="depart" placeholder="Lieu de départ" required>
+         <input type="text" name="Destination" placeholder="destination" required>
+          <button type="submit">voir prix</button>
+       </form>
+     </div>
+       <div class="right">
+         <p class="explore">Explorer d'autres moyens de transports</p>
+         <img src="C:\Users\aliou\OneDrive\Pictures\Travel like a VIP in Detroit – experience the excellence of VIP RideWay chauffeur services!.jpeg" alt="chauffeur taxi avec Client">
+        </div> 
+ </div>
+
+
+
+    
+    
